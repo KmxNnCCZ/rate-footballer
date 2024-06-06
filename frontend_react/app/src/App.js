45e867b428a0dd47b4268e41a10c59256a186c57
ああああああ <mmmm48698062@icloud.com>
2024-06-06 19:56:52 +0900

    react-router-domの導入

--- frontend_react/app/src/App.js
+++ frontend_react/app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Top } from './routes/Top';
+import { Login } from './routes/Login';
+import { SignUp } from './routes/SignUp';
+import { Notfound } from './routes/Notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ChakraProvider>
+    <h1>react-router-dom</h1>
+    <Routes>
+      <Route path="/" element={ <Top /> }></Route>
+      <Route path="login" element={ <Login /> }></Route>
+      <Route path="signUp" element={ <SignUp /> }></Route>
+      <Route path="*" element={ <Notfound /> } />
+    </Routes>
+   </ChakraProvider>
   );
 }
 
---
+++ frontend_react/app/src/routes/SignUp.js
@@ -0,0 +1,7 @@
+export const SignUp = () => {
+  return (
+    <div>
+      <p>新規登録ページ</p>
+    </div>
+  )
+}
